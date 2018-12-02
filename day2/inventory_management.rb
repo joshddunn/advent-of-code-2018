34b318d49394722f1f424504707f08e71c6dfd71
Josh Dunn <joshdunn.uwo@gmail.com>
2018-12-02 16:25:04 -0500

    day 1 and 2

--- day2/inventory_management.rb
+++ day2/inventory_management.rb
@@ -2,36 +2,32 @@ require_relative './input.rb'
 
 class InventoryManagement
 
-  def initialize
-    @two_instances = 0
-    @three_instances = 0
-  end
-
-  def checksum
+  def self.checksum
+    two_instances = 0
+    three_instances = 0
     INPUT.each do |input|
       letter_count = Hash.new(0)
       input.each_char do |letter|
         letter_count[letter] += 1
       end
       occurrences = letter_count.values
-      @two_instances   += 1 if occurrences.include? 2
-      @three_instances += 1 if occurrences.include? 3
+      two_instances   += 1 if occurrences.include? 2
+      three_instances += 1 if occurrences.include? 3
     end
 
-    @two_instances * @three_instances
+    two_instances * three_instances
   end
 
-  def common_letters
-    string_codes = Hash.new(0)
-    INPUT.each do |input|
-      characters = input.split('')
+  def self.common_letters
+    string_codes = {}
+    INPUT.each do |characters|
       characters.length.times do |index|
-        karacters = characters.dup
-        karacters.delete_at(index)
-        string_codes["#{index}:#{karacters.join('')}"] += 1
+        karacters = characters[0...index] + characters[index+1..characters.length]
+        key = "#{index}:#{karacters}"
+        return karacters if string_codes[key]
+        string_codes[key] = true
       end
     end
-    string_codes.find { |_, val| val > 1 }.first.split(':').last
   end
 
 end
--- day2/run.rb
+++ day2/run.rb
@@ -1,4 +1,4 @@
 require_relative './inventory_management.rb'
 
-puts "Part 1: The checksum is #{InventoryManagement.new.checksum}"
-puts "Part 2: The common letters are #{InventoryManagement.new.common_letters}"
+puts "Part 1: The checksum is #{InventoryManagement.checksum}"
+puts "Part 2: The common letters are #{InventoryManagement.common_letters}"
