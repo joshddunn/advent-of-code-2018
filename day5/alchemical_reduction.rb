aa03052b52cc233fc700c9799b633bb67e1a1081
joshddunn <joshdunn.uwo@gmail.com>
2018-12-05 00:52:42 -0500

    day 5 optimized

--- day5/alchemical_reduction.rb
+++ day5/alchemical_reduction.rb
@@ -2,23 +2,27 @@ require_relative './input.rb'
 
 class AlchemicalReduction
 
-  def polymer(initial = nil, final = INPUT)
-    alphabet = ('a'..'z')
-    until initial == final
-      initial = final
-      alphabet.each do |letter|
-        final = final.gsub("#{letter}#{letter.upcase}", '')
-        final = final.gsub("#{letter.upcase}#{letter}", '')
+  def polymer(letter = nil)
+    input = INPUT.split('')
+    unreducible = []
+
+    input.each do |i|
+      next if i.downcase == letter
+      if unreducible.length == 0 || i == unreducible.last || i.downcase != unreducible.last.downcase
+        unreducible.push i
+      else
+        unreducible.pop
       end
     end
-    final.length
+
+    unreducible.length
   end
 
   def shortest
     alphabet = ('a'..'z')
     values = []
     alphabet.each do |letter|
-      values.push polymer(nil, INPUT.gsub(/[#{letter}#{letter.upcase}]/, ''))
+      values.push polymer(letter)
     end
     values.min
   end
